4685efce01f9a0d1c5f672c21c009d657d8262da
ksh980212 <ksh980212@gmail.com>
2019-12-30 21:24:54 +0900

    mysql 연동

--- src/main/java/com/record/springboot/domain/record/RecordRepository.java
+++
@@ -1,6 +0,0 @@
-package com.record.springboot.domain.record;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecordRepository extends JpaRepository<Record, Long> {
-}
