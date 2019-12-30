6d602de2cedce89e777f19753a7ab38b70b6eae8
ksh980212 <ksh980212@gmail.com>
2019-12-30 20:07:45 +0900

    Load record desc 처리

--- src/main/java/com/record/springboot/domain/record/RecordRepository.java
+++ src/main/java/com/record/springboot/domain/record/RecordRepository.java
@@ -3,5 +3,4 @@ package com.record.springboot.domain.record;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RecordRepository extends JpaRepository<Record, Long> {
-
 }
--- src/main/java/com/record/springboot/web/RecordController.java
+++ src/main/java/com/record/springboot/web/RecordController.java
@@ -1,5 +1,30 @@
 package com.record.springboot.web;
 
+import com.record.springboot.domain.record.Record;
+import com.record.springboot.service.RecordService;
+import com.record.springboot.web.dto.RecordLoadResponseDto;
+import com.record.springboot.web.dto.RecordSaveRequestDto;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
 public class RecordController {
 
+  private final RecordService recordService;
+
+  @PostMapping("/api/v1/record")
+  public Long save(@RequestBody RecordSaveRequestDto requestDto){
+    return recordService.save(requestDto);
+  }
+
+  @GetMapping("/api/v1/record")
+  public List<Record> findAll(Sort sort){
+    return recordService.findAll(sort);
+  }
 }
